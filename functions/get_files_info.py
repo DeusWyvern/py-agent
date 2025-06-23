7719edf44edbb5cebc393c4318cc4195fff83928
DeusWyvern <dpefley98@gmail.com>
2025-06-23 13:22:13 -0700

    Function to limit scope of agent

---
+++ functions/get_files_info.py
@@ -0,0 +1,31 @@
+import os
+
+
+def get_files_info(working_directory, directory=None):
+    if directory is None:
+        directory = "."
+
+    abs_working = os.path.abspath(working_directory)
+    if os.path.isabs(directory):
+        full_path = directory
+    else:
+        full_path = os.path.abspath(os.path.join(abs_working, directory))
+
+    if not full_path.startswith(abs_working):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    elif not os.path.isdir(full_path):
+        return f'Error: "{directory}" is not a directory'
+
+    try:
+        file_list = os.listdir(full_path)
+        size_list = list(map(lambda x: get_path_size(x, full_path), file_list))
+        return "\n".join(size_list)
+    except Exception as e:
+        return f"Error encountered listing files: {e}"
+
+
+def get_path_size(file, path):
+    file_path = os.path.join(path, file)
+    file_size = os.path.getsize(file_path)
+    file_is_dir = os.path.isdir(file_path)
+    return f"- {file}: file_size={file_size} bytes, is_dir={file_is_dir}"
